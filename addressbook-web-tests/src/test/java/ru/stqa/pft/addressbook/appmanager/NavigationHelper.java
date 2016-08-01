c3869b3d574da9f73dd2d9e90e8ebcb040d02bd1
Sergey <serg3d@hotmail.com>
2016-08-01 00:32:25 -0700

    2.11
    homework 5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -6,18 +6,18 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by IEUser on 7/31/2016.
  */
-public class NavigationHelper {
+public class NavigationHelper extends HelperBase{
     private FirefoxDriver wd;
 
     public NavigationHelper(FirefoxDriver wd) {
-        this.wd = wd;
+        super (wd);
     }
 
     public void gotoGroupPage() {
-        wd.findElement(By.linkText("groups")).click();
+        click(By.linkText("groups"));
     }
 
     public void gotoHomePage() {
-        wd.findElement(By.linkText("home")).click();
+        click(By.linkText("home"));
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -6,23 +6,20 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by IEUser on 7/31/2016.
  */
-public class SessionHelper {
+public class SessionHelper extends HelperBase{
 
 
     private FirefoxDriver wd;
 
     public SessionHelper(FirefoxDriver wd) {
-        this.wd = wd;
+        super(wd);
 
     }
 
     public void login(String username, String password) {
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+        type(By.name("user"), username);
+        type(By.name("pass"), password);
+        click(By.xpath("//form[@id='LoginForm']/input[3]"));
+
     }
 }
