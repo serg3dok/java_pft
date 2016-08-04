2febd3c4a9e19df6e6673009a2974fc2c871e8ff
Sergey <serg3d@hotmail.com>
2016-08-04 00:59:17 -0700

    3.9

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -8,17 +8,25 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  * Created by IEUser on 7/31/2016.
  */
 public class NavigationHelper extends HelperBase{
-    private WebDriver wd;
+
 
     public NavigationHelper(WebDriver wd) {
         super (wd);
     }
 
     public void gotoGroupPage() {
+        if (isElementPresent(By.tagName("h1")) &&
+                wd.findElement(By.tagName("h1")).getText().equals("Groups") &&
+                isElementPresent(By.name("new"))) {
+            return;
+        }
         click(By.linkText("groups"));
     }
 
     public void gotoHomePage() {
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        }
         click(By.linkText("home"));
     }
 }
